2140484ffe412a5175a7c68ddca52fc889942e81
Debmalya Jash <debmalya.jash@gmail.com>
2016-04-29 01:28:46 +0800

    Added test for performance

--- src/test/java/oce/gift/JobStateMapTest.java
+++ src/test/java/oce/gift/JobStateMapTest.java
@@ -37,5 +37,30 @@ public class JobStateMapTest {
 		actual = jsm.getExperience("Amazon");
 		Assert.assertEquals(0.00f, actual, 0.00f);
 	}
+	
+	@Test
+	public final void testPerformance() {
+		JobStateMap jsm = new JobStateMap();
+		long startTime = System.currentTimeMillis();
+		for (int i = 0; i < 1000000; i++) {
+			jsm.addExperience("Avita Software Private Limited", 6.5f);
+			
+			float actual = jsm.getExperience("Avita Software Private Limited");
+			Assert.assertEquals(6.5f, actual, 0.00f);
+			
+			actual = jsm.getExperience("Avita Software Private");
+			Assert.assertEquals(0.00f, actual, 0.00f);
+			
+			actual = jsm.getExperience("Google");
+			Assert.assertEquals(0.00f, actual, 0.00f);
+			
+			actual = jsm.getExperience("IBM");
+			Assert.assertEquals(0.00f, actual, 0.00f);
+			
+			actual = jsm.getExperience("Amazon");
+			Assert.assertEquals(0.00f, actual, 0.00f);
+		}
+		System.out.println("Time taken :" + (System.currentTimeMillis() - startTime) + " ms.");
+	}
 
 }
