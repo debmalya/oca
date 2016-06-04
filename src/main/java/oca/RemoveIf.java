3267ca78fc186389e1139de861927a75d151d45b
Debmalya Jash <debmalya.jash@gmail.com>
2016-06-04 16:23:00 +0800

    Testing new method removeIf with predicate.(Java8)

---
+++ src/main/java/oca/RemoveIf.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package oca;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author debmalyajash
+ *
+ */
+public class RemoveIf {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		check(2);
+
+	}
+	
+	/**
+	 * First generate random numbers in range 0 and 1000.
+	 * 
+	 * @param n whose multiple if any will be removed.
+	 */
+	public static void check(int n){
+		List<Integer> randomIntCollection = new ArrayList<>();
+		Random random = new Random();
+		for (int i = 0; i < 10; i++) {
+			randomIntCollection.add(random.nextInt(1000));
+		}
+		System.out.println("Original collection :" + randomIntCollection);
+		randomIntCollection.removeIf(e -> e % n == 0);
+		System.out.println("After removing multiple of " + n + " :" + randomIntCollection);
+	}
+
+}
